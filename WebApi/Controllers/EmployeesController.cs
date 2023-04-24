2dda8f5d24f838962b2664057b23df94f1c16cd2
Marek Żołubak <mjrmaru@gmail.com>
2023-04-24 18:29:57 +0200

    Add employees controller

---
+++ WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Database;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ILogger<EmployeesController> _logger;
+
+        public EmployeesController(ILogger<EmployeesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Get Employees")]
+        public IActionResult Get(int? id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var query = db.Employees;
+                if (id.HasValue)
+                {
+                    query.Where(x => x.Id == id);
+                }
+                var employees = query.ToList();
+                if (!employees.Any())
+                {
+                    return new NotFoundResult();
+                }
+                return new OkObjectResult(employees);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Employee request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            if (string.IsNullOrWhiteSpace(request.FirstName) ||
+                string.IsNullOrWhiteSpace(request.LastName) ||
+                string.IsNullOrWhiteSpace(request.CarRegistrationNumber) ||
+                !request.BirthDate.HasValue)
+            {
+                return new BadRequestObjectResult("All fields have to be filled in!");
+            }
+
+            using (var db = new DatabaseContext())
+            {
+                var employee = new EmployeeEntity
+                {
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    CarRegistrationNumber = request.CarRegistrationNumber,
+                    BirthDate = request.BirthDate.Value
+                };
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            return new OkResult();
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Employee request, int id)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            
+            using (var db = new DatabaseContext())
+            {
+                var employee = db.Employees.FirstOrDefault(x => x.Id == id);
+                if (employee == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                if (!string.IsNullOrWhiteSpace(request.FirstName))
+                {
+                    employee.FirstName = request.FirstName; 
+                }
+                if (!string.IsNullOrWhiteSpace(request.LastName))
+                {
+                    employee.LastName = request.LastName;
+                }
+                if (!string.IsNullOrWhiteSpace(request.CarRegistrationNumber))
+                {
+                    employee.CarRegistrationNumber = request.CarRegistrationNumber;
+                }
+                if (request.BirthDate.HasValue)
+                {
+                    employee.BirthDate = request.BirthDate.Value;
+                }
+                db.Update(employee);
+                db.SaveChanges();
+            }
+
+            return new OkResult();
+        }
+    }
+}
---
+++ WebApi/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string CarRegistrationNumber { get; set; }
+    }
+}
