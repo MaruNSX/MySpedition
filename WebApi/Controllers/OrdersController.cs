63d10fc64727dd2b9b40ec24de878167e468263e
Marek Żołubak <mjrmaru@gmail.com>
2023-04-24 18:57:47 +0200

    Add some comments to employee controllers

--- WebApi/Controllers/OrdersController.cs
+++ WebApi/Controllers/OrdersController.cs
@@ -19,6 +19,7 @@ namespace WebApi.Controllers
         [HttpGet]
         public IActionResult Get(int? id)
         {
+            _logger.LogInformation($"Fetching orders with id: {id}");
             using (var db = new DatabaseContext())
             {
                 var query = db.Orders.AsSingleQuery();
@@ -29,8 +30,10 @@ namespace WebApi.Controllers
                 var orders = query.AsNoTracking().Include(x => x.Employee).ToList();
                 if (!orders.Any())
                 {
+                    _logger.LogInformation("There was no orders to return.");
                     return new NotFoundResult();
                 }
+                _logger.LogInformation($"Returning {orders.Count} orders");
                 return new OkObjectResult(orders);
             }
         }
@@ -40,6 +43,7 @@ namespace WebApi.Controllers
         {
             if (request == null)
             {
+                _logger.LogError("Could not proceed with request as it was empty.");
                 throw new ArgumentNullException(nameof(request));
             }
             if (!request.StartTime.HasValue ||
@@ -47,6 +51,11 @@ namespace WebApi.Controllers
                 !request.Price.HasValue ||
                 !request.EmployeeId.HasValue)
             {
+                _logger.LogError($"Some of input parameters are not filled in: Parameters:" +
+                    $"StartTime: {request.StartTime}," +
+                    $"EndTime: {request.EndTime}," +
+                    $"Price: {request.Price}," +
+                    $"EmployeeId: {request.EmployeeId}");
                 return new BadRequestObjectResult("All fields have to be filled in!");
             }
 
@@ -55,8 +64,10 @@ namespace WebApi.Controllers
                 var employee = db.Employees.FirstOrDefault(x => x.Id == request.EmployeeId);
                 if (employee == null) 
                 {
+                    _logger.LogError($"Could not find employee with id {request.EmployeeId}.");
                     return new BadRequestObjectResult($"Could not find employee with id: {request.EmployeeId}.");
                 }
+                _logger.LogInformation("Creating new order");
                 var order = new OrderEntity
                 {
                     StartTime = request.StartTime.Value,
@@ -75,27 +86,33 @@ namespace WebApi.Controllers
         {
             if (request == null)
             {
+                _logger.LogError("Could not proceed with request as it was empty.");
                 throw new ArgumentNullException(nameof(request));
             }
 
             using (var db = new DatabaseContext())
             {
+                _logger.LogInformation($"Fetching order with id {id}");
                 var order = db.Orders.FirstOrDefault(x => x.Id == id);
                 if (order == null)
                 {
+                    _logger.LogError($"There is no order with id: {id}.");
                     return new NotFoundObjectResult($"Could not find order with id: {id}.");
                 }
 
                 if (request.StartTime.HasValue)
                 {
+                    _logger.LogInformation($"Assigning start time: {request.StartTime}");
                     order.StartTime = request.StartTime.Value;
                 }
                 if (request.EndTime.HasValue)
                 {
+                    _logger.LogInformation($"Assigning end time: {request.EndTime}");
                     order.EndTime = request.EndTime.Value;
                 }
                 if (request.Price.HasValue)
                 {
+                    _logger.LogInformation($"Assigning price: {request.Price}");
                     order.Price = request.Price.Value;
                 }
                 if (request.EmployeeId.HasValue)
@@ -103,11 +120,13 @@ namespace WebApi.Controllers
                     var employee = db.Employees.FirstOrDefault(x => x.Id == request.EmployeeId);
                     if (employee == null)
                     {
+                        _logger.LogError($"Could not find employee with id {request.EmployeeId}");
                         return new BadRequestObjectResult($"Could not find employee with id: {request.EmployeeId}.");
                     }
-
+                    _logger.LogInformation("Assigning employee.");
                     order.Employee = employee;
                 }
+                _logger.LogInformation("Updating employee");
                 db.Update(order);
                 db.SaveChanges();
             }
