c31e3c971a18f2aa5934e51d31345335e92f89c3
Marek Żołubak <mjrmaru@gmail.com>
2023-05-13 13:13:11 +0200

    Expose application to local network

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Net.Sockets;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+string localIP = LocalIPAddress();
+
+app.Urls.Add("http://" + localIP + ":5072");
+app.Urls.Add("https://" + localIP + ":7072");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +30,21 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+static string LocalIPAddress()
+{
+    using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+    {
+        socket.Connect("8.8.8.8", 65530);
+        IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
+        if (endPoint != null)
+        {
+            return endPoint.Address.ToString();
+        }
+        else
+        {
+            return "127.0.0.1";
+        }
+    }
+}
